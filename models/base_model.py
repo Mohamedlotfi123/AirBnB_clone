ee99e0b87df14957082da9531c053945984b379a
root <root@DESKTOP-8VTPROF>
2024-05-19 15:44:09 +0300

    airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel():
         id: unique id "string".
         created_at: current datetime when an instance is created.
         updated_at: current datetime when an instance is updated.
-    
+
     Methods:
         save: Update the public instance attribute updated_at.
         to_dict: Return dictionary containing all keys/values of __dict__
@@ -29,8 +29,10 @@ class BaseModel():
             self.id = kwargs.get('id')
             self.name = kwargs.get('name')
             self.my_number = kwargs.get('my_number')
-            self.created_at = datetime.strptime(kwargs.get('created_at'), Format)
-            self.updated_at = datetime.strptime(kwargs.get('updated_at'), Format)
+            c = kwargs.get('created_at')
+            self.created_at = datetime.strptime(c, Format)
+            c = kwargs.get('updated_at')
+            self.updated_at = datetime.strptime(c, Format)
         else:
             self.name = None
             self.my_number = 0
@@ -44,13 +46,13 @@ class BaseModel():
         to print instance of a class
         """
         return f"[BaseModel] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """
         Update the public instance attribute updated_at with
         the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
