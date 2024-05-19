a27da2dcec78e73f89f2b824ec7bd88505bd0e93
root <root@DESKTOP-8VTPROF>
2024-05-19 15:33:30 +0300

    airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ BaseModel Class """
+from datetime import datetime
+from uuid import uuid4
+
 
 class BaseModel():
     """
@@ -13,7 +16,49 @@ class BaseModel():
     
     Methods:
         save: Update the public instance attribute updated_at.
-        to_dict: Dictionary containing all keys/values of __dict__
+        to_dict: Return dictionary containing all keys/values of __dict__
                 of the instance.
         __str__: Print [<class name>] (<self.id>) <self.__dict__>
     """
+    def __init__(self, *args, **kwargs):
+        """
+        initialization method
+        """
+        if len(kwargs) > 0:
+            Format = "%Y-%m-%dT%H:%M:%S.%f"
+            self.id = kwargs.get('id')
+            self.name = kwargs.get('name')
+            self.my_number = kwargs.get('my_number')
+            self.created_at = datetime.strptime(kwargs.get('created_at'), Format)
+            self.updated_at = datetime.strptime(kwargs.get('updated_at'), Format)
+        else:
+            self.name = None
+            self.my_number = 0
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        print [<class name>] (<self.id>) <self.__dict__> when you try
+        to print instance of a class
+        """
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        """
+        Update the public instance attribute updated_at with
+        the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        dic = self.__dict__
+        dic["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dic["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dic["__class__"] = "BaseModel"
+        return dic
